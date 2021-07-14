ba60c5a95c8fcff6fbc04ebf8e6361a875d78354
YYdesoul <yanyusoul@gmail.com>
2021-07-14 10:29:25 +0200

    update ajax

---
+++ springmvc-06-ajax02/src/main/java/com/soul/controller/AjaxController.java
@@ -0,0 +1,25 @@
+package com.soul.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+public class AjaxController {
+    @RequestMapping("/t1")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping("/a1")
+    public void a1(String name, HttpServletResponse response) throws IOException {
+        System.out.println("a1:param=> " + name);
+        if ("Jack".equals(name)) {
+            response.getWriter().print("true");
+        } else {
+            response.getWriter().print("false");
+        }
+    }
+}
